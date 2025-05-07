9138624cdba34c245ca6ac9459b74dddfe06b2aa
Ana_Rode <anarode.rodrigues@gmail.com>
2025-05-06 23:19:02 -0300

    Updates BarrierType and EnvironmentType enums

--- code/src/main/java/com/barreirasapp/model/enums/EnvironmentType.java
+++ code/src/main/java/com/barreirasapp/model/enums/EnvironmentType.java
@@ -1,3 +1,14 @@
-public enum EnumTipoAmbiente {
+package com.barreirasapp.model.enums;
 
+public enum EnvironmentType {
+    CLASSROOM,
+    LIBRARY,
+    LABORATORY,
+    HALLWAY,
+    COURT,
+    PATIO,
+    BATHROOM,
+    CAFETERIA,
+    SCHOOL_TRANSPORT,
+    OTHER;
 }
